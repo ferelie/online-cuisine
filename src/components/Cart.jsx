f86cbbbdb5f9dcee30cc3cfa12f37b09c186dfdf
ferelie <abdallah.yasser0501@gmail.com>
2024-08-03 22:33:46 +0300

    finished cart modal and quantity logic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../context/CartContext";
 import UserProgressContext from "../context/UserProgressContext";
-import {currencyFormatter} from "../utils/currencyFormatter";
+import { currencyFormatter } from "../utils/currencyFormatter";
 import Button from "./UI/Button";
 
 const Cart = () => {
@@ -11,23 +11,38 @@ const Cart = () => {
 
     const cartTotal = cartCtx.items.reduce((currentNumber, item) => {
         return currentNumber + item.price * 20 * item.quantity;
-    },0);
+    }, 0);
 
+    const handleHideCart = () => {
+        userProgressCtx.hideCart();
+    };
 
     return (
         <Modal className="cart" isOpen={userProgressCtx.progress === "cart"}>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map((item) => (
-                    <li key={item.id}>
-                        {item.name} - {item.price*20}
+                    <li key={item.id} className="cart-item">
+                        <p>
+                            {item.name} - {item.quantity} x {" "}
+                            {currencyFormatter.format(item.price * 20)}
+                        </p>
+                        <p className="cart-item-actions">
+                            <button onClick={() => cartCtx.removeItem(item.id)}>-</button>
+                            <span>{item.quantity}</span>
+                            <button onClick={() => cartCtx.addItem(item)}>+</button>
+                        </p>
                     </li>
                 ))}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
-                <Button>Go to Checkout</Button>
+                <Button textOnly onClick={handleHideCart}>
+                    Close
+                </Button>
+                <Button onClick={userProgressCtx.checkout}>
+                    Go to Checkout
+                </Button>
             </p>
         </Modal>
     );
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children, isOpen, className=""}) => {
-    const dialog = useRef()
+const Modal = ({ children, isOpen, className = "" }) => {
+  
+    const dialog = useRef();
+
     useEffect(() => {
-      if (isOpen) {
-        dialog.current.showModal();
-      }
-    ;
-    }, [isOpen])
-    
+        const modal = dialog.current;
+        if (isOpen) {
+            modal.showModal();
+        }
+        return () => modal.close();
+    }, [isOpen]);
 
     return createPortal(
-        <dialog ref={dialog} className={`modal ${className}`}> {children}</dialog>,
+        <dialog ref={dialog} className={`modal ${className}`}>
+            {" "}
+            {children}
+        </dialog>,
         document.getElementById("modal")
     );
 };
